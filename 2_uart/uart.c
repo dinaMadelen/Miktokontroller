6dafd932343d4dc4f7ce6f69eaa6af134c217f53
dinaMadelen <dmshegna@gmail.com>
2024-04-18 08:49:45 +0200

    include function definitions

--- 2_uart/uart.c
+++ 2_uart/uart.c
@@ -2,42 +2,42 @@
 #include "uart.h"
 
 #define UART ((NRF_UART_REGS*)0x40002000)
+
 typedef struct {
-	volatile uint32_t TASKS_STARTRX;
-	volatile uint32_t TASKS_STOPRX;
-	volatile uint32_t TASKS_STARTTX;
-	volatile uint32_t TASKS_STOPTX;
-    volatile uint32_t RESERVED1[3];
-	volatile uint32_t TASKS_SUSPEND;
+	volatile uint32_t STARTRX;
+    volatile uint32_t STOPRX;
+    volatile uint32_t STARTTX;
+    volatile uint32_t STOPTX;
+    volatile uint32_t RESERVED0[3];
+    volatile uint32_t SUSPEND;
     volatile uint32_t RESERVED1[56];
-	volatile uint32_t EVENTS_CTS;
-	volatile uint32_t EVENTS_NCTS;
-	volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t CTS;
+    volatile uint32_t NCTS;
+    volatile uint32_t RXDRDY;
     volatile uint32_t RESERVED2[4];
-	volatile uint32_t EVENTS_TXDRDY;
+    volatile uint32_t TXDRDY;
     volatile uint32_t RESERVED3[1];
-	volatile uint32_t EVENTS_ERROR;
-	volatile uint32_t RESERVED4[7];
-	volatile uint32_t EVENTS_RXTO;
-	volatile uint32_t RESERVED5[46];
-	volatile uint32_t SHORTS;
-    volatile uint32_t RESERVED6[64];
+    volatile uint32_t ERROR;
+    volatile uint32_t RESERVED4[7];
+    volatile uint32_t RXTO;
+    volatile uint32_t RESERVED5[110];
+    volatile uint32_t INTEN;
     volatile uint32_t INTENSET;
     volatile uint32_t INTENCLR;
-    volatile uint32_t RESERVED7[93];
+    volatile uint32_t RESERVED6[93];
     volatile uint32_t ERRORSRC;
-    volatile uint32_t RESERVED8[31];
+    volatile uint32_t RESERVED7[31];
     volatile uint32_t ENABLE;
-    volatile uint32_t RESERVED9[1];
+    volatile uint32_t RESERVED8[1];
     volatile uint32_t PSELRTS;
     volatile uint32_t PSELTXD;
     volatile uint32_t PSELCTS;
     volatile uint32_t PSELRXD;
     volatile uint32_t RXD;
     volatile uint32_t TXD;
-    volatile uint32_t RESERVED10[1];
+    volatile uint32_t RESERVED9[1];
     volatile uint32_t BAUDRATE;
-    volatile uint32_t RESERVED11[17];
+    volatile uint32_t RESERVED10[17];
     volatile uint32_t CONFIG;
 }NRF_UART_REG;
 
