716da11eb70bf14373c83016cfda2e391f492cc6
mariesatre <mariesatre@outlook.com>
2024-04-18 10:34:30 +0200

    jobb

--- 3_gpiote/gpiote.h
+++ 3_gpiote/gpiote.h
@@ -6,19 +6,22 @@
 
 typedef struct {
 	// Tasks
-	volatile uint32_t OUT[4];
+	volatile uint32_t TASKS_OUT[8];
+	volatile uint32_t RESERVED0[4];
+	volatile uint32_t TASKS_SET[8];
+	volatile uint32_t RESERVED1[4];
+	volatile uint32_t TASKS_CLR[8];
+	volatile uint32_t RESERVED2[32];
 	// Events
-	volatile uint32_t RESERVED0[60];
-	volatile uint32_t IN[4];
-	volatile uint32_t RESERVED1[27];
-	volatile uint32_t PORT;
+	volatile uint32_t EVENTS_IN[8];
+	volatile uint32_t RESERVED3[23];
+	volatile uint32_t EVENTS_PORT;
 	// Registers
-	volatile uint32_t RESERVED2[96];
-	volatile uint32_t INTEN;
+	volatile uint32_t RESERVED4[97];
 	volatile uint32_t INTENSET;
 	volatile uint32_t INTENCLR;
-	volatile uint32_t RESERVED3[129];
-	volatile uint32_t CONFIG[4];
+	volatile uint32_t RESERVED5[129];
+	volatile uint32_t CONFIG[8];
 } NRF_GPIOTE_REG;
 
 #endif
